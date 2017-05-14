475fb07d35bd7325286c8f17129ccbbb7b2f3b3a
bobaa48 <bobaa48@gmail.com>
2017-05-14 20:41:20 +0900

    "django-test"

--- photowall/views.py
+++ photowall/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+    'DB에서 Post목록을 가져와서, HTML문자열을 생성/응답합니다.'
+    return render(request, 'photowall/index.html', {
+            'post_list': Post.objects.all(),
+})
